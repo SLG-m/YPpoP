26b9b9b15d44255df9aed69fcb8f90d2cfe9299d
Jesus <glokhl@mail.ru>
2025-03-17 12:24:08 +0300

    1

---
+++ лабы/1 4 вариант/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Firearm
+{
+    public string Name { get; set; }
+    public float Caliber { get; set; }
+    public double Range { get; set; }
+
+    public Firearm(string name, float caliber, double range)
+    {
+        Name = name;
+        Caliber = caliber;
+        Range = range;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Название: {Name}");
+        Console.WriteLine($"Калибр: {Caliber} мм");
+        Console.WriteLine($"Дальность: {Range} м");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
