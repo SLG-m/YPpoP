3a48ff4074b6a2169e8dff6363f76ba66bb5947c
Jesus <glokhl@mail.ru>
2025-03-26 23:07:10 +0300

    7 lab 2 zadanie ? worck   ? - (есть вопрос),  -? возможно если вставлять предложение с тире то тиро будет считать отдельным словом (я пока не разбирался)

--- лабы/7/ConsoleApp1/ConsoleApp1/Program.cs
+++ лабы/7/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,29 +1,55 @@
 ﻿using System;
-using System.Text;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        Random random = new Random();
-        StringBuilder result = new StringBuilder(10);
+        Console.WriteLine("Введите предложение:");
+        string sentence = Console.ReadLine();
 
-        for (int i = 0; i < 10; i++)
+        Console.WriteLine("Введите номер K (начинается с 1):");
+        int K = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите номер N (начинается с 1):");
+        int N = int.Parse(Console.ReadLine());
+
+        // Разбиваем предложение на слова (учитываем несколько пробелов)
+        string[] words = sentence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (words.Length == 0)
         {
-            if (i % 2 == 0) // Нечетная позиция (индексация с 0)
-            {
-                // Добавляем случайную букву (английскую)
-                char letter = (char)random.Next('a', 'z' + 1);
-                result.Append(letter);
-            }
-            else // Четная позиция
-            {
-                // Добавляем случайную четную цифру
-                int evenDigit = random.Next(0, 5) * 2; // 0, 2, 4, 6, 8
-                result.Append(evenDigit);
-            }
+            Console.WriteLine("Предложение не содержит слов!");
+            return;
         }
 
-        Console.WriteLine("Результат: " + result.ToString());
+        // Проверяем корректность введенных K и N
+        if (K < 1 || K > words.Length || N < 1 || N > words.Length)
+        {
+            Console.WriteLine($"Ошибка: K и N должны быть от 1 до {words.Length}");
+            return;
+        }
+
+        // Меняем первое слово (индекс 0) с K-ым (индекс K-1)
+        if (K != 1) // Если K не равно 1, иначе менять не нужно
+        {
+            string temp = words[0];
+            words[0] = words[K - 1];
+            words[K - 1] = temp;
+        }
+
+        // Меняем последнее слово (индекс words.Length-1) с N-ым (индекс N-1)
+        if (N != words.Length) // Если N не равно последнему индексу, иначе менять не нужно
+        {
+            string temp = words[words.Length - 1];
+            words[words.Length - 1] = words[N - 1];
+            words[N - 1] = temp;
+        }
+
+        // Собираем слова обратно в предложение
+        string result = string.Join(" ", words);
+
+        Console.WriteLine("Результат:");
+        Console.WriteLine(result);
     }
 }
--- лабы/7/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ лабы/7/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94236939e44354c2c8b69a8476cc297e5f7503a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bebf5014eeda89f221ab6d680aba0b168bd909dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
