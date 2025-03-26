bebf5014eeda89f221ab6d680aba0b168bd909dd
Jesus <glokhl@mail.ru>
2025-03-26 23:00:31 +0300

    7 lab zadanie 1 work

---
+++ лабы/7/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        StringBuilder result = new StringBuilder(10);
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0) // Нечетная позиция (индексация с 0)
+            {
+                // Добавляем случайную букву (английскую)
+                char letter = (char)random.Next('a', 'z' + 1);
+                result.Append(letter);
+            }
+            else // Четная позиция
+            {
+                // Добавляем случайную четную цифру
+                int evenDigit = random.Next(0, 5) * 2; // 0, 2, 4, 6, 8
+                result.Append(evenDigit);
+            }
+        }
+
+        Console.WriteLine("Результат: " + result.ToString());
+    }
+}
