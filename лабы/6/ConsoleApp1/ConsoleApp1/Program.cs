ef8b555f37396605c4eace4e1e32f32e88757de0
Jesus <glokhl@mail.ru>
2025-03-26 10:07:34 +0300

    6 lab 1 zadanie worck

---
+++ лабы/6/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Пример вещественного массива
+        double[] array = { 1.5, 2.3, 3.7, 4.1, 5.9, 6.2, 7.4, 8.0, 9.6, 10.8 };
+
+        // Вычисление среднего арифметического
+        double sum = 0;
+        foreach (double num in array)
+        {
+            sum += num;
+        }
+        double average = sum / array.Length;
+
+        // Подсчет элементов, меньших среднего
+        int count = 0;
+        foreach (double num in array)
+        {
+            if (num < average)
+            {
+                count++;
+            }
+        }
+
+        // Вывод результата
+        Console.WriteLine($"Среднее арифметическое: {average}");
+        Console.WriteLine($"Количество элементов меньше среднего: {count}");
+    }
+}
