1f18feaaecf66d84e2e61c4153cdd62f589a3cb4
Jesus <glokhl@mail.ru>
2025-03-27 12:13:46 +0300

    8 labs finali

--- лабы/8/ConsoleApp1/ConsoleApp1/Program.cs
+++ лабы/8/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,61 +1,55 @@
 ﻿using System;
 using System.IO;
 using System.Text.RegularExpressions;
-using System.Threading;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Устанавливаем белый фон консоли
         Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
         Console.Clear();
 
         try
         {
-            // Чтение файла из корневой папки
             string filePath = "text.txt";
             string text = File.ReadAllText(filePath);
 
             Console.WriteLine("Исходный текст:");
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine(text);
-            Console.WriteLine("\nОбработанный текст:");
 
-            // Обработка текста
+            Console.WriteLine("\nОбработанный текст:");
             ProcessText(text);
         }
         catch (FileNotFoundException)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Файл text.txt не найден в корневой папке приложения.");
-            Console.ResetColor();
         }
         catch (Exception ex)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+        finally
+        {
             Console.ResetColor();
         }
-
-        Console.ResetColor();
         Console.WriteLine("\nНажмите любую клавишу для выхода...");
         Console.ReadKey();
     }
 
     static void ProcessText(string text)
     {
-        // Регулярное выражение для аббревиатур (заглавные буквы и цифры, длина 2+ символа)
-        Regex acronymRegex = new Regex(@"\b[A-ZА-Я0-9]{2,}\b");
-        // Регулярное выражение для слов с заглавной буквы
+        Regex acronymRegex = new Regex(@"\b[A-ZА-Я]{2,}\b");
         Regex capitalizedRegex = new Regex(@"\b[A-ZА-Я][a-zа-я]+\b");
-        // Регулярное выражение для чисел (целых и дробных)
         Regex numberRegex = new Regex(@"\b\d+\.?\d*\b");
 
         int lastPos = 0;
 
-        // Находим все совпадения всех регулярных выражений
-        var matches = acronymRegex.Matches(text)
-            .Cast<Match>()
+        var matches = acronymRegex.Matches(text).Cast<Match>()
             .Concat(capitalizedRegex.Matches(text).Cast<Match>())
             .Concat(numberRegex.Matches(text).Cast<Match>())
             .OrderBy(m => m.Index)
@@ -63,36 +57,31 @@ class Program
 
         foreach (var match in matches)
         {
-            // Выводим текст до совпадения обычным цветом
             if (match.Index > lastPos)
             {
-                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Black;
                 Console.Write(text.Substring(lastPos, match.Index - lastPos));
             }
 
-            // Определяем тип совпадения и устанавливаем соответствующий цвет
             if (acronymRegex.IsMatch(match.Value))
             {
-                Console.ForegroundColor = ConsoleColor.Red; // Аббревиатуры - красным
+                Console.ForegroundColor = ConsoleColor.Red;
             }
             else if (capitalizedRegex.IsMatch(match.Value))
             {
-                Console.ForegroundColor = ConsoleColor.Green; // Слова с заглавной буквы - зеленым
+                Console.ForegroundColor = ConsoleColor.Green;
             }
             else if (numberRegex.IsMatch(match.Value))
             {
-                Console.ForegroundColor = ConsoleColor.Blue; // Числа - синим
+                Console.ForegroundColor = ConsoleColor.Blue;
             }
-
-            // Выводим совпадение
             Console.Write(match.Value);
             lastPos = match.Index + match.Length;
         }
 
-        // Выводим оставшийся текст обычным цветом
         if (lastPos < text.Length)
         {
-            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.Write(text.Substring(lastPos));
         }
     }
--- лабы/8/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ лабы/8/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e98dfe87357ce6a23a5cfbbafb6a24dfaaa7bb49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db21e0bc01735d45dac07e5bfcd46de7a6a8292e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
