35c699b027ffb2bb72baa6887dc7a8d76e6052bd
Jesus <glokhl@mail.ru>
2025-03-27 12:22:24 +0300

    8 laba такая же что и прошлый коммит но тут нет лишних библиотек

--- лабы/8/ConsoleApp1/ConsoleApp1/Program.cs
+++ лабы/8/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.IO;
 using System.Text.RegularExpressions;
-using System.Linq;
-
 class Program
 {
     static void Main(string[] args)
--- лабы/8/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ лабы/8/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db21e0bc01735d45dac07e5bfcd46de7a6a8292e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f18feaaecf66d84e2e61c4153cdd62f589a3cb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
