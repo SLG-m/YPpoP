d02457212508810fae89ef925f483248b39e5c70
Jesus <glokhl@mail.ru>
2025-04-21 13:08:48 +0300

    34 там лимит увеличен

--- лабы/34/WinFormsApp1/WinFormsApp1/Forms/Form1.Designer.cs
+++ лабы/34/WinFormsApp1/WinFormsApp1/Forms/Form1.Designer.cs
@@ -33,21 +33,24 @@
             button2 = new Button();
             button3 = new Button();
             button4 = new Button();
+            groupBox1 = new GroupBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // dataGridView1
             // 
-            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Location = new Point(12, 12);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(776, 322);
+            dataGridView1.Size = new Size(776, 374);
             dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(60, 389);
+            button1.Location = new Point(17, 17);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 1;
@@ -57,7 +60,7 @@
             // 
             // button2
             // 
-            button2.Location = new Point(165, 389);
+            button2.Location = new Point(98, 17);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 2;
@@ -67,7 +70,7 @@
             // 
             // button3
             // 
-            button3.Location = new Point(288, 389);
+            button3.Location = new Point(179, 17);
             button3.Name = "button3";
             button3.Size = new Size(75, 23);
             button3.TabIndex = 3;
@@ -77,27 +80,38 @@
             // 
             // button4
             // 
-            button4.Location = new Point(396, 389);
+            button4.Location = new Point(260, 17);
             button4.Name = "button4";
             button4.Size = new Size(75, 23);
             button4.TabIndex = 4;
             button4.Text = "Обновить";
             button4.UseVisualStyleBackColor = true;
-            button4.Click += this.button4_Click;
+            button4.Click += button4_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Location = new Point(12, 392);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(339, 46);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(groupBox1);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -108,5 +122,6 @@
         private Button button2;
         private Button button3;
         private Button button4;
+        private GroupBox groupBox1;
     }
 }
